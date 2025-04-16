4c94e7bc561491ba01759dfbd783ee0a4918c161
Luis Rosales <conejoconsal@hotmail.com>
2025-04-15 23:45:37 -0600

    sdf

--- conejo-negro-1.js
+++ conejo-negro-1.js
@@ -7,3 +7,12 @@ const esProgramador = true;
 console.log("Nombre:", nombre);
 console.log("Edad:", edad);
 console.log("¿Es programador?:", esProgramador);
+
+// Crear un elemento h1
+const h1 = document.createElement("h1");
+
+// Asignar texto al h1
+h1.textContent = "Hola, soy Luis";
+
+// Inyectar el h1 en el body
+document.body.appendChild(h1);
