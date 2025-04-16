a450a44c55de831ae7cde225397607a36ad5c971
Luis Rosales <conejoconsal@hotmail.com>
2025-04-15 23:19:44 -0600

    primer commit

---
+++ conejo-negro-1.js
@@ -0,0 +1,9 @@
+// Definición de variables sencillas
+const nombre = "Luis";
+const edad = 30;
+const esProgramador = true;
+
+// Mostrar las variables en la consola
+console.log("Nombre:", nombre);
+console.log("Edad:", edad);
+console.log("¿Es programador?:", esProgramador);
